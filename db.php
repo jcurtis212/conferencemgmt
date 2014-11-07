0108dcf189b905613a2357ad4f11ed788e8ab594
Justin Patriquin <justinpatriquin@gmail.com>
2014-11-06 20:47:34 -0400

    added some base files to get this project started

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+  function connect(){
+    $conf = readConfig();
+
+    return mysql_connect($conf->{'host'}, $conf->{'user'}, $conf->{'pass'});
+  }
+
+  function readConfig(){
+    $file = fopen("dbconfig.json", "r");
+    $json = fread($file, filesize("dbconfig.json"));
+
+    return json;
+  }
+?>
