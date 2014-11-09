ff3e88e4e6128dc877035f89fb491182b491d8b8
Tim Arrott <tarrott2012@gmail.com>
2014-11-09 12:07:07 -0400

    Added a login and register page
    As well as a navbar in order to quickly jump to them

--- header.php
+++ header.php
@@ -1,3 +1,12 @@
 <?php
 
 ?>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<img src="images/logo-xs.jpg" alt="logo-sm" id="logo-home" />
+<ul class="nav nav-pills" role="tablist">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Conferences</a></li>
+  <li role="presentation"><a href="login.php">Login</a></li>
+  <li role="presentation"><a href="register.php">Register</a></li>
+</ul>
--- index.php
+++ index.php
@@ -2,6 +2,23 @@
   require 'header.php'
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <meta charset="utf-8">
+        <meta name="description" content="Conference Management Organization System">
+
+        <title>4confs</title>
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+ 		<link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+
+    </head>
+    <body>
+    </body>
+</html>
+
 <?php
   require 'footer.php'
 ?>
